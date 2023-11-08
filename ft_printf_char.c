929836ee0f9a6518ef0ff68d192d5b356861049e
Julien Houyet <jhouyet@shi-r6-p7.s19.be>
2023-11-08 14:17:51 +0100

    Fix random francinette

--- ft_printf_char.c
+++ ft_printf_char.c
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 08:47:08 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/11/08 13:48:29 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/11/08 14:10:28 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	ft_printf_char(va_list arg)
 	char	c;
 	
 	c = va_arg(arg, int);
-	if (c < 0)
-		return (-1);
 	ft_putchar_fd(c, 1);
 	return (1);
 }
